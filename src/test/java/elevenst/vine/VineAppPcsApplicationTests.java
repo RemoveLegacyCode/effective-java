eef066434310311cce10c1e492e49e743b268c8c
yunstorm <yunstorm@gmail.com>
2022-04-26 17:53:16 +0900

    gradle 세팅

---
+++ src/test/java/elevenst/vine/VineAppPcsApplicationTests.java
@@ -0,0 +1,13 @@
+package elevenst.vine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VineAppPcsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/elevenst/vine/pcs/domain/LowestPriceBOTest.java
@@ -0,0 +1,18 @@
+package elevenst.vine.pcs.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class LowestPriceBOTest {
+
+    @DisplayName("크롤링데이터가 있는 상품인 경우 크롤링데이터로 적용")
+    @Test
+    void listOfTest() {
+
+    }
+}
